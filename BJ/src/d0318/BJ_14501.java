ac44af109bec25f773ffe4ad963633cea3e0eed5
gyh8781 <gyh8781@gmail.com>
2022-03-19 03:45:41 +0900

    [0318] 백준14501 퇴사

---
+++ BJ/src/d0318/BJ_14501.java
@@ -0,0 +1,44 @@
+package d0318;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_14501 {
+
+    static int[][] schedule;
+    static int answer, n;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        schedule = new int[n + 1][2];
+        answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            schedule[i][0] = Integer.parseInt(st.nextToken());
+            schedule[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(1, 0, 0);
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int start, int cost, int profit) {
+
+        if (start > n + 1) {
+            answer = Math.max(answer, profit);
+            return;
+        }
+
+        if(start==n+1){
+            answer = Math.max(answer, profit+cost);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            dfs(i + schedule[i][0], schedule[i][1], profit+cost);
+        }
+    }
+}
